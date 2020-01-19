80363b126db7fad2c2f0a39097b615f157d26610
fritzgt <tuanis@live.com>
2020-01-19 08:16:31 +0200

    Initial commit

---
+++ Photo/Photo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo
+//
+//  Created by FGT MAC on 1/19/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
